d8a909787bf849d95488584146e0f89297094cfa
Daniela Bastias <danybastias@gmail.com>
2024-09-24 08:13:59 -0300

    Realizando cambios

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-// Seleccionamos el botón y el menú
-const navToggle = document.querySelector('.nav-toggle');
-const menu = document.querySelector('.opciones');
+// main.js
+document.addEventListener("DOMContentLoaded", function() {
+    const navToggle = document.querySelector(".nav-toggle");
+    const opciones = document.querySelector(".opciones");
 
-// Evento de clic para alternar el menú
-navToggle.addEventListener('click', () => {
-    menu.classList.toggle('show-menu');
+    navToggle.addEventListener("click", function() {
+        opciones.classList.toggle("show-menu");
+    });
 });
+<script>
+    window.addEventListener('load', function() {
+        document.body.style.visibility = 'visible'
+    });
+</script>
