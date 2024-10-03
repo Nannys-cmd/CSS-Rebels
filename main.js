c91614705634faa78b9c655a8c82601ccd894da6
Daniela Bastias <danybastias@gmail.com>
2024-10-02 22:37:41 -0300

    Error en la rama main

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 // Asegúrate de que este script se ejecute después de que el DOM se haya cargado
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     const navToggle = document.querySelector('.nav-toggle');
-    const navMenu = document.querySelector('.opciones');
+    const opciones = document.querySelector('.opciones');
 
     navToggle.addEventListener('click', () => {
-        console.log('Hamburguesa clickeada'); // Agrega esto para depuración
-        navMenu.classList.toggle('show-menu');
+        opciones.classList.toggle('show-menu');
     });
 });
